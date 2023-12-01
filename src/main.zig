50058d76c61bd62f0ae2615802a7ada73ee1a559
Diogo-V <diogo.venancio00@gmail.com>
2023-12-01 18:19:37 +0000

    chore: add 1

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,34 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("Hello {s}\n", .{"Diogo"});
-}
+    const file = try std.fs.cwd().openFile("input.txt", .{});
+    defer file.close();
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    var bufReader = std.io.bufferedReader(file.reader());
+    var inStream = bufReader.reader();
+
+    var sum: u32 = 0;
+    var buf: [1024]u8 = undefined;
+    while (try inStream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        var firstNumber: i8 = -1;
+        var lastNumber: i8 = -1;
+
+        // Loops over each character in the line and finds the first and last numbers
+        for (line) |char| {
+            if (char >= '1' and char <= '9') {
+                if (firstNumber == -1) {
+                    firstNumber = @as(i8, @intCast(char - '0'));
+                }
+                lastNumber = @as(i8, @intCast(char - '0'));
+            }
+        }
+
+        // Concatenates the first and last numbers into a single number
+        const number: i8 = firstNumber * 10 + lastNumber;
+
+        // Adds the number to the sum
+        sum += @as(u32, @intCast(number));
+    }
+
+    std.debug.print("sum: {}\n", .{sum});
 }
